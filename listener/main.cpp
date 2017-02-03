bf812ba7270b2feb4684980380e27885725f828f
Daniele Murroni <daniele.murroni@gmail.com>
2017-02-03 17:24:51 +0000

    first commit

---
+++ listener/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  listener
+//
+//  Created by Daniele Murroni on 03/02/2017.
+//  Copyright © 2017 Daniele Murroni. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+#include <stdint.h>
+#include <amqp_tcp_socket.h>
+#include <amqp.h>
+#include <amqp_framing.h>
+
+#include <assert.h>
+
+#include <utils.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
